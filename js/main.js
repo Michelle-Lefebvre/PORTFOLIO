14a8817a450ede481ff07c152e562980dab6c0ad
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-09-01 14:01:15 -0400

    clean-up all code and validate

--- js/main.js
+++ js/main.js
@@ -3,29 +3,3 @@ function onClickMenu() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
-
-// function onClickProjectInfo() {
-//     $('.collapse').collapse()
-//     document.getElementById("collapse").classList.toggle("projInfo");
-//     document.getElementById("collapse").classList.toggle("collapse");
-//     document.getElementById("right").classList.toggle("projInfo");
-//     document.getElementById("top3d").classList.toggle("projInfo");
-//     document.getElementById("bottom").classList.toggle("projInfo");
-//     document.getElementById("back").classList.toggle("projInfo"); 
-// }
-
-
-// initialize bootstrap toggle
-// $('#collapse').collapse({
-//     toggle: false
-//   })
-
-//   initialize bootstrap all tooltips
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip()
-//   })
-
-// modal animation
-// $('#myModal').on('shown.bs.modal', function () {
-//     $('#myInput').trigger('focus')
-//   })
