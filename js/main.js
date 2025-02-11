583997e11ad836343893651dc5f2998f5f23c528
Michelle Lefebvre <mmldesigns007@gmail.com>
2025-02-11 11:08:24 -0500

    add new project page with carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 function onClickMenu() {
-    document.getElementById("menu").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
+  document.getElementById("menu").classList.toggle("change")
+  document.getElementById("nav").classList.toggle("change")
+  document.getElementById("menu-bg").classList.toggle("change-bg")
 }
+// carousel
+let items = document.getElementsByClassName("item")
+
+var baseTransform = "translateX(-50%) translateY(-50%)"
+
+for (let item of items) {
+  item.addEventListener("click", function (event) {
+    moveTo(event.target.dataset.index)
+  })
+}
+
+function moveTo(toIndex) {
+  for (let item of items) {
+    let itemIndex = item.dataset.index
+    let delta = itemIndex - toIndex
+    item.style.transform =
+      baseTransform +
+      " translateZ(-" +
+      Math.abs(delta) * 500 +
+      "px) translateX(" +
+      delta * 100 +
+      "px)"
+  }
+}
+
+moveTo(3)
